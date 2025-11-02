b0cd4e9fa4b111b77da6d08679ba93f534a32e1e
Igor <xxxigorbiesiada1@gmail.com>
2025-11-02 21:07:53 +0100

    starting pagination

--- charity/views.py
+++ charity/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from charity.models import Donation, Institution
-
+from django.core.paginator import Paginator
 # Create your views here.
 
 class LandingPageView(TemplateView):
@@ -13,6 +13,14 @@ class LandingPageView(TemplateView):
         context['count_donation'] = Donation.count_bags()
         context['count_institution'] = Institution.objects.count()
         
+        context['fundacja'] = Institution.objects.filter(type="fundacja")
+        context['organizacja'] = Institution.objects.filter(type="organizajca")
+        context['zbiorka'] = Institution.objects.filter(type="zbiorka")
+
+
+
+
+
         return context
 
 class AddDonationView(TemplateView):
