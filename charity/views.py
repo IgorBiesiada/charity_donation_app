cea6acef570617e62f9816a31dbcc4b8f2e921d0
Igor <xxxigorbiesiada1@gmail.com>
2025-12-28 00:13:12 +0100

    pagination but not finished

--- charity/views.py
+++ charity/views.py
@@ -14,13 +14,23 @@ def landing_page(request):
     zbiorka = Institution.objects.filter(type="zbiorka")
     
     fundacja_paginator = Paginator(fundacja, 5)
+    organizacja_paginator = Paginator(organizacja, 5)
+    zbiorka_paginator = Paginator(zbiorka, 5)
+    
+    fun_page_num = request.GET.get('fun_page')
+    org_page_num = request.GET.get('org_page')
+    zbi_page_num = request.GET.get('zbi_page')
+    
+    fundacja_page = fundacja_paginator.get_page(fun_page_num)
+    organizacja_page = organizacja_paginator.get_page(org_page_num)
+    zbiorka_page = zbiorka_paginator.get_page(zbi_page_num)
     
     
     context = { 'count_donation': count_donation,
                 'count_institution': count_institution,
-                'fundacja': fundacja,
-                'organizacja': organizacja, 
-                'zbiorka': zbiorka
+                'fundacja': fundacja_page,
+                'organizacja': organizacja_page, 
+                'zbiorka': zbiorka_page
                }
     
     return render(request, 'base.html', context)
